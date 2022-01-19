f850cfe70165aeaa2be6b07d12034d6d60c95a8b
Shinorikishi <kadamrohitsandeep@gmail.com>
2022-01-19 11:12:47 +0530

    Favourite Movie Assignment submission

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -1,7 +1,15 @@
 const movies = [];
 
 function favouriteMovie(operation, movie) {
-    // Write your code here
+    // Write your code hereW
+    if (operation==="add")
+    {
+        movies.push(movie)
+    }
+    else{
+        movies.pop()
+    }
+    return movies
 }
 
 module.exports = favouriteMovie;
